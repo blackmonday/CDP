eb898b35be09bc7142912095b5704874b2843662
Larry <larry@blackmondaydesign.com>
2025-05-20 22:57:59 +0100

    First commit

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,40 @@
 module.exports = {
 
-  // Insert values here
-
-}
+  "organisations": [
+    {
+      name: "Organisation One",
+      type_tag: "Buyer",
+      type: "Central government",
+      PPON_number: "0192837465",
+      date_of_registration: "12 December 1998"
+    },
+    {
+      name: "Organisation Two",
+      type_tag: "Supplier",
+      type: "Central government",
+      PPON_number: "0192837465",
+      date_of_registration: "12 December 1998"
+    },
+    {
+      name: "Organisation Three",
+      type_tag: "Individual",
+      type: "Central government",
+      PPON_number: "0192837465",
+      date_of_registration: "12 December 1998"
+    },
+    {
+      name: "Organisation Four",
+      type_tag: "SME",
+      type: "Central government",
+      PPON_number: "0192837465",
+      date_of_registration: "12 December 1998"
+    },
+    {
+      name: "Organisation Five",
+      type_tag: "Supplier/Buyer",
+      type: "Central government",
+      PPON_number: "0192837465",
+      date_of_registration: "12 December 1998"
+    },
+  ],
+};
