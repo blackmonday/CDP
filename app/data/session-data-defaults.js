d0dabc0224a3a4c2aea604eb94c2ec2abb8b8861
Larry <larry@blackmondaydesign.com>
2025-06-02 13:53:52 +0100

    2 June updates

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -4,52 +4,67 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Buyer",
+      type_tag: "Central",
       type: "Central government",
       PPON_number: "0192837465",
-      date_of_registration: "12 December 1998"
+      date_of_registration: "12 December 2025",
+      address: "30 Guild Street, London, SE1 3SY"
     },
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Supplier",
-      type: "Central government",
+      type_tag: "MCSE",
+      type: "Supplier",
       PPON_number: "0192837465",
-      date_of_registration: "12 December 1998"
+      date_of_registration: "12 December 2025",
+      address: "Floor 4, Brampton House, Red Lion Square, London, WC1R 4RAY"
     },
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Supplier/Buyer",
+      type_tag: "Sub-central",
       type: "Central government",
       PPON_number: "0192837465",
-      date_of_registration: "12 December 1998"
-    },
-    {
-      name: "Organisation FouNamer",
-      url: "organisation",
-      type_tag: "Buyer",
-      type: "Central government",
-      PPON_number: "0192837465",
-      date_of_registration: "12 December 1998"
+      date_of_registration: "12 December 2025",
+      address: "421 Wellingborough Road, Northampton, NN1 4EY"
     },
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Supplier",
+      type_tag: "Other",
       type: "Central government",
       PPON_number: "0192837465",
-      date_of_registration: "12 December 1998"
+      date_of_registration: "12 December 2025",
+      address: "7 Bridge Road, London, N22 7SN"
+    },
+    {
+      name: "Organisation Name",
+      url: "organisation",
+      type_tag: "SME",
+      type: "Supplier",
+      PPON_number: "0192837465",
+      date_of_registration: "12 December 2025",
+      address: "12a-12c, Claret Court, 125 Connersville Way, Croydon, CR0 4FT"
+    },
+    {
+      name: "Organisation Name",
+      url: "organisation",
+      type_tag: "Other",
+      type: "Supplier",
+      PPON_number: "0192837465",
+      date_of_registration: "12 December 2025",
+      address: "12a-12c, Claret Court, 125 Connersville Way, Croydon, CR0 4FT"
     },
   ],
   "Organisation One": [
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Buyer",
+      type_tag: "Buyer - MCSE",
       type: "Central government",
       PPON_number: "0192837465",
-      date_of_registration: "12 December 1998"
+      date_of_registration: "12 December 2025",
+      address: "30 Guild Street, London, SE1 3SY"
     },
   ],
 };
