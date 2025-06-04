4f7540792e0c6f0befcddb3f6a6742d890e2ec10
Larry <larry@blackmondaydesign.com>
2025-06-04 12:39:03 +0100

    PPON updates after UCD review

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -4,8 +4,8 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Central",
-      type: "Central government",
+      tag: "Buyer",
+      org_type: "Central",
       PPON_number: "0192837465",
       date_of_registration: "12 December 2025",
       address: "30 Guild Street, London, SE1 3SY"
@@ -13,8 +13,8 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "MCSE",
-      type: "Supplier",
+      tag: "Supplier",
+      org_type: "MCSE",
       PPON_number: "0192837465",
       date_of_registration: "12 December 2025",
       address: "Floor 4, Brampton House, Red Lion Square, London, WC1R 4RAY"
@@ -22,8 +22,8 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Sub-central",
-      type: "Central government",
+      tag: "Buyer",
+      org_type: "Sub-central",
       PPON_number: "0192837465",
       date_of_registration: "12 December 2025",
       address: "421 Wellingborough Road, Northampton, NN1 4EY"
@@ -31,8 +31,8 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Other",
-      type: "Central government",
+      tag: "Buyer",
+      org_type: "Other",
       PPON_number: "0192837465",
       date_of_registration: "12 December 2025",
       address: "7 Bridge Road, London, N22 7SN"
@@ -40,8 +40,8 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "SME",
-      type: "Supplier",
+      tag: "Supplier",
+      org_type: "SME",
       PPON_number: "0192837465",
       date_of_registration: "12 December 2025",
       address: "12a-12c, Claret Court, 125 Connersville Way, Croydon, CR0 4FT"
@@ -49,8 +49,8 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Other",
-      type: "Supplier",
+      tag: "Supplier",
+      org_type: "Other",
       PPON_number: "0192837465",
       date_of_registration: "12 December 2025",
       address: "12a-12c, Claret Court, 125 Connersville Way, Croydon, CR0 4FT"
@@ -60,8 +60,8 @@ module.exports = {
     {
       name: "Organisation Name",
       url: "organisation",
-      type_tag: "Buyer - MCSE",
-      type: "Central government",
+      tag: "Buyer",
+      org_type: "Central",
       PPON_number: "0192837465",
       date_of_registration: "12 December 2025",
       address: "30 Guild Street, London, SE1 3SY"
