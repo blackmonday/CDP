a82d4f69d4ffe6edd5aa74a0f50d04be39d01f34
Larry <larry@blackmondaydesign.com>
2025-06-20 09:44:54 +0100

    created to MVP varions of PPON

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -6,7 +6,7 @@ module.exports = {
       url: "organisation",
       tag: "Buyer",
       org_type: "Central",
-      PPON_number: "0192837465",
+      PPON_number: "8325891717834903",
       date_of_registration: "12 December 2025",
       address: "30 Guild Street, London, SE1 3SY"
     },
@@ -15,7 +15,7 @@ module.exports = {
       url: "organisation",
       tag: "Supplier",
       org_type: "MCSE",
-      PPON_number: "0192837465",
+      PPON_number: "1959054418741424",
       date_of_registration: "12 December 2025",
       address: "Floor 4, Brampton House, Red Lion Square, London, WC1R 4RAY"
     },
@@ -24,7 +24,7 @@ module.exports = {
       url: "organisation",
       tag: "Buyer",
       org_type: "Sub-central",
-      PPON_number: "0192837465",
+      PPON_number: "5456729279426748",
       date_of_registration: "12 December 2025",
       address: "421 Wellingborough Road, Northampton, NN1 4EY"
     },
@@ -33,7 +33,7 @@ module.exports = {
       url: "organisation",
       tag: "Buyer",
       org_type: "Other",
-      PPON_number: "0192837465",
+      PPON_number: "3443838827375779",
       date_of_registration: "12 December 2025",
       address: "7 Bridge Road, London, N22 7SN"
     },
@@ -42,7 +42,7 @@ module.exports = {
       url: "organisation",
       tag: "Supplier",
       org_type: "SME",
-      PPON_number: "0192837465",
+      PPON_number: "5895545748061769",
       date_of_registration: "12 December 2025",
       address: "12a-12c, Claret Court, 125 Connersville Way, Croydon, CR0 4FT"
     },
@@ -51,7 +51,7 @@ module.exports = {
       url: "organisation",
       tag: "Supplier",
       org_type: "Other",
-      PPON_number: "0192837465",
+      PPON_number: "2323044586290528",
       date_of_registration: "12 December 2025",
       address: "12a-12c, Claret Court, 125 Connersville Way, Croydon, CR0 4FT"
     },
@@ -62,7 +62,7 @@ module.exports = {
       url: "organisation",
       tag: "Buyer",
       org_type: "Central",
-      PPON_number: "0192837465",
+      PPON_number: "0619430298769021",
       date_of_registration: "12 December 2025",
       address: "30 Guild Street, London, SE1 3SY"
     },
@@ -71,7 +71,7 @@ module.exports = {
     {
       name: "The Big Org Name",
       PPON_number: "DRFI-1747-YZZC-EW",
-      companies_house_number: "0123456789",
+      companies_house_number: "7135137459182484",
       registry: "Charity Commission for England and Wales",
       registry_number: "9876543210",
       email: "email@organisation.com",
